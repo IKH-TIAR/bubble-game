91dc08ddb6bc151e4f556730e472a41126b4920f
IKHTIAR <ikhti407@gmail.com>
2024-03-04 21:58:42 +0600

    hit and timer function added

--- script.js
+++ script.js
@@ -1,8 +1,32 @@
-let bubbles ="";
-
-for (let i = 0; i < 147; i++) { 
-    let rn = Math.floor(Math.random() * 10) + 1;
-    bubbles += `<div class="bubble">${rn}</div>`;
+function makeBubble() {  
+    let bubbles ="";
+    
+    for (let i = 0; i < 147; i++) { 
+        let rn = Math.floor(Math.random() * 10) + 1;
+        bubbles += `<div class="bubble">${rn}</div>`;
+    }
+    
+    document.querySelector("#bottom-panel").innerHTML = bubbles;
 }
 
-document.querySelector("#bottom-panel").innerHTML = bubbles;
+function newHit() {
+    let rn = Math.floor(Math.random() * 10) + 1;
+    document.querySelector("#hitvalue").textContent = rn;
+}
+
+
+function timer() {
+    let timevalue = 60;
+    let timerfunc = setInterval(function () { 
+        if (timevalue >= 0) {
+            document.querySelector("#timevalue").textContent = timevalue;
+            timevalue--;
+        } else {
+            clearInterval(timerfunc);
+        }
+    }, 1000);
+}
+
+makeBubble();
+newHit();
+timer();
