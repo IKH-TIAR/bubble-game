46d586a344885d25570705fac818b45944196896
IKHTIAR <ikhti407@gmail.com>
2024-03-04 23:49:51 +0600

    Functionality added

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let score = 0;
+let hitValue = 0;
+
 function makeBubble() {  
     let bubbles ="";
     
@@ -10,22 +13,37 @@ function makeBubble() {
 }
 
 function newHit() {
-    let rn = Math.floor(Math.random() * 10) + 1;
-    document.querySelector("#hitvalue").textContent = rn;
+    hitValue = Math.floor(Math.random() * 10) + 1;
+    document.querySelector("#hitvalue").textContent = hitValue;
 }
 
 
 function timer() {
-    let timevalue = 60;
+    let timevalue = 3;
     let timerfunc = setInterval(function () { 
         if (timevalue >= 0) {
             document.querySelector("#timevalue").textContent = timevalue;
             timevalue--;
         } else {
             clearInterval(timerfunc);
+            document.querySelector("#bottom-panel").innerHTML = `<h1>Game Over</h1>`;
         }
     }, 1000);
 }
+
+function updateScore() {
+    score += 10;
+    document.querySelector("#scorevalue").textContent = score;
+}
+
+document.querySelector("#bottom-panel").addEventListener("click", function (event) {
+    let clicedValue = Number(event.target.textContent);
+    if (clicedValue == hitValue) {
+        updateScore();
+        newHit();
+        makeBubble();
+    }
+});
 
 makeBubble();
 newHit();
